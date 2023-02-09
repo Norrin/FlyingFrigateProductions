9d9fea644cd34485a58f1237aef25045828b066b
Shane Cleavelin <shane.cleavelin@showseeker.com>
2023-02-09 15:17:42 -0500

    Main-Page | First Draft

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
+import Gallery from "./components/Gallery";
+import Blog from "./components/Blog";
 
 function App() {
   //Render the application
@@ -12,6 +14,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="gallery" element={<Gallery />} />
+        <Route path="blog" element={<Blog />} />
         <Route path="about/*" element={<About />} />
       </Routes>
     </>
