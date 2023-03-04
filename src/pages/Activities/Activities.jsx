8460797148225fcb55c8d8e937a65de9f9bc27d9
Shane Cleavelin <shane.cleavelin@showseeker.com>
2023-03-04 14:35:50 -0500

    R1 | Remove Images and baseline

--- src/pages/Activities/Activities.jsx
+++ src/pages/Activities/Activities.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Page } from "@components";
+import { Maze } from "@pages";
 
 const Activities = () => {
    return (
       <Page>
          <div>Activitites</div>
+         <Maze></Maze>
       </Page>
    );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import Home from "./Home";
 import Activities from "./Activities/Activities";
+import Maze from "./Activities/Maze";
 import Gallery from "./Gallery";
 import Blog from "./Blog";
 import About from "./About";
 
-export { Home, Activities, Gallery, Blog, About };
+export { Home, Activities, Maze, Gallery, Blog, About };
